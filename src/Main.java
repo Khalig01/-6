3414fa849444e3b99a9331721632725d63c4c198
Khal01 <aliyev_56@list.ru>
2023-10-17 01:24:24 +0300

    дз6

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+     public static void main(String[] args) {
+
+        System.out.println("Задача1");
+        for (int i =1;i<=10 ; i =i+1) {
+             System.out.println(i );
+        }
+         System.out.println("Задача2");
+         for (int i =10; i>0; i =i-1) {
+             System.out.println(i);
+         }
+         System.out.println("Задача3");
+         for (int i =0;i<=17 ; i =i+2) {
+             System.out.println(i );
+         }
+         System.out.println("Задача4");
+         for (int i =10;i>=-10 ; i =i-1) {
+             System.out.println(i );
+         }
+         System.out.println("Задача5");
+         for (int i =1904;i<=2096 ; i =i+4) {
+             System.out.println("Год является високосным"+i );
+         }
+         System.out.println("Задача6");
+         for (int i =7;i<=98 ; i =i + 7) {
+             System.out.println(i );
+         }
+         System.out.println("Задача7");
+         for (int i =1;i<=512 ; i =i * 2) {
+             System.out.println(i );
+         }
+         System.out.println("Задача7");
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+    }
+
